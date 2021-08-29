88ec966c5b1beb1afd0fa23e62759264da08c244
Wafelowski <heavywolfpl@gmail.com>
2021-08-29 14:31:41 +0200

    screw 2.0, make it 3.0

--- PFE/PFE.cs
+++ PFE/PFE.cs
@@ -16,7 +16,7 @@ namespace PFE
 		public override string Author { get; } = "Wafel & Cyanox";
 		public override string Name { get; } = "PeanutFckingExplodes";
 		public override string Prefix { get; } = "PFE";
-		public override Version Version { get; } = new Version(2, 0, 0);
+		public override Version Version { get; } = new Version(3, 0, 0);
 		public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
 
 
