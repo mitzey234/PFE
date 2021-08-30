d05f96c0f637b8abcabe63ccb6a4cae9337ed578
Wafelowski <heavywolfpl@gmail.com>
2021-08-30 10:31:36 +0200

    fix explosion not happening

--- PFE/PlayerEvents.cs
+++ PFE/PlayerEvents.cs
@@ -23,13 +23,10 @@ namespace PFE
 			{
 				try
                 {
-					//float y = ev.Target.Position.y + 4;
-					//Vector3 pos = new Vector3(ev.Target.Position.x, y, ev.Target.Position.z); 
 					for (int i = 0; i < Plugin.Singleton.Config.Magnitude; i++)
 					{
 						new ExplosiveGrenade(ItemType.GrenadeHE, ev.Target) { FuseTime = Plugin.Singleton.Config.Delay }.SpawnActive(ev.Target.Position, ev.Target);
-						//new ExplosiveGrenade(ItemType.GrenadeHE, ev.Target) { FuseTime = Plugin.Singleton.Config.Delay }.SpawnActive(pos, ev.Target);
-						Log.Debug($"SCP-173 has exploded (pos goes here).", Plugin.Singleton?.Config?.Debug ?? false);
+						Log.Debug($"SCP-173 has exploded ({ev.Target.Position}) with a {Plugin.Singleton.Config.Delay} delay.", Plugin.Singleton?.Config?.Debug ?? false);
 					}
 				}
 				catch (Exception e)
