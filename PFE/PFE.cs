cfdcfb75c5ff8f8acaae05db2cbfb66e5f672763
Alexander Young <alex1001@live.ca>
2021-12-30 21:34:51 -0700

    Updated to v4

--- PFE/PFE.cs
+++ PFE/PFE.cs
@@ -11,31 +11,34 @@ namespace PFE
 		public override string Prefix { get; } = "PFE";
 		public override Version Version { get; } = new Version(3, 1, 0);
 		public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
-
+		private bool state = false;
 
 		public static Plugin Singleton;
 		public PlayerEvents PlayerEvents;
 
 		public override void OnEnabled() 
 		{
-			if (!Config.IsEnabled) return;
-
-			base.OnEnabled();
+			if (state) return;
 
 			Singleton = this;
 			PlayerEvents = new PlayerEvents(this);
 
             Handlers.Player.Dying += PlayerEvents.OnPlayerDeath;
+
+			state = true;
+			base.OnEnabled();
 		}
 
 		public override void OnDisabled() 
 		{
-			base.OnDisabled();
+			if (!state) return;
 
 			Handlers.Player.Dying -= PlayerEvents.OnPlayerDeath;
 
 			PlayerEvents = null;
 
+			state = false;
+			base.OnDisabled();
 		}
 	}
 }
